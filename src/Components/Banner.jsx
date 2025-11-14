f38d46f0b9fc6a4f2150c5895b4acef342c757a3
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2025-11-14 11:02:31 +0100

    Uncomment overlay div in Banner component

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <>
       <div className="banner d-flex align-items-center justify-content-center text-center text-white">
-        {/* <div className="overlay"></div> */}
+        <div className="overlay"></div>
         <div className="content position-relative">
           <h1 className="display-4 fw-bolder">Mitochondrial Eve Journal of Humanities Postgraduate Studies</h1>
           <p className="lead">a publication of the faculty of Humanities, imo State university, Owerri, Nigeria</p>
